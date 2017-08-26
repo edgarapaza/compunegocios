7f7a7c585b390810ae79884a78531b80fdee4845
Edgar Apaza <edgarapazac@gmail.com>
2017-08-26 10:57:25 -0500

    Modificnado index

--- public/index.php
+++ public/index.php
@@ -6,35 +6,30 @@
 
 				<form action="Controllers/login.controller.php" method="POST" class="form-horizontal" role="form">
 
-				<div class="alert alert-danger respuesta" style="display: none;">
-					<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-					<strong id="msg"></strong>
-				</div>
+					<div class="form-group">
+						<legend>Ingreso al sistema</legend>
+					</div>
 
-						<div class="form-group">
-							<legend>Ingreso al sistema</legend>
+					<div class="form-group">
+						<label for="inputUsuario" class="col-sm-2 control-label">Usuario:</label>
+						<div class="col-sm-10">
+							<input type="text" name="user" id="inputUsuario" class="form-control" required="required">
 						</div>
+					</div>
 
-						<div class="form-group">
-							<label for="inputUsuario" class="col-sm-2 control-label">Usuario:</label>
-							<div class="col-sm-10">
-								<input type="text" name="user" id="inputUsuario" class="form-control" required="required">
-							</div>
+					<div class="form-group">
+						<label for="inputClave" class="col-sm-2 control-label">Clave:</label>
+						<div class="col-sm-10">
+							<input type="password" name="clave" id="inputClave" class="form-control" required="required">
 						</div>
+					</div>
 
-						<div class="form-group">
-							<label for="inputClave" class="col-sm-2 control-label">Clave:</label>
-							<div class="col-sm-10">
-								<input type="password" name="clave" id="inputClave" class="form-control" required="required">
-							</div>
+
+					<div class="form-group">
+						<div class="col-sm-10 col-sm-offset-2">
+							<button type="submit" class="btn btn-primary" id="btnIngreso">Ingresar</button>
 						</div>
-
-
-						<div class="form-group">
-							<div class="col-sm-10 col-sm-offset-2">
-								<button type="submit" class="btn btn-primary" id="btnIngreso">Ingresar</button>
-							</div>
-						</div>
+					</div>
 				</form>
 			</div>
 		</div>
