fe90cb18e7b6a37b275805cc94d2e98cc9b7e3c8
Edgar Apaza <edgarapazac@gmail.com>
2018-05-02 23:09:34 -0500

    Sitio limpiado- Reemplazado por la nueva plantilla

--- doc/tablaCompra.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE compra(
-	idcompra int not null auto_increment PRIMARY KEY,
-	idproducto int not null,
-	idproveedor int not null,
-	numFactura varchar(100),
-	fecEmision date,
-	idalmacen int not null
-);
