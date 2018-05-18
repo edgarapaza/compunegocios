80d3dd86cd31e216687496b60894a9c688a79473
Edgar Apaza <edgarapazac@gmail.com>
2018-05-18 15:19:41 -0500

    Limpieaza 2

--- Controllers/listadoforCompras.controller.php
+++
@@ -1,9 +0,0 @@
-<?php 
-//INSERT INTO compraprovedor VALUES (NULL,idproducto,codigo,idproveedor,cantidad,pvp,numfact,now(),idpersonal,idregistro);
-$idproducto  = $_REQUEST['idproducto'];
-$codigo      = $_REQUEST['codigo'];
-$idproveedor = $_REQUEST['idproveedor'];
-$idregistro  = $_REQUEST['idregistro'];
-?>
-
-
